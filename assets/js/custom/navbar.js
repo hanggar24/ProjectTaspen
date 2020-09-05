360e71bc14845dfbf27cd58661097f8e7312f810
hanggar24 <hanggar.w.prayogo@alfadigital.id>
2020-09-05 13:53:55 +0700

    add info keuangan

--- assets/js/custom/navbar.js
+++ assets/js/custom/navbar.js
@@ -1,15 +1,11 @@
-
-
-
 $(document).ready(function () {
 
 
 
-$("#btn-nav").click(function () {
+  $("#btn-nav").click(function () {
     $(".button-toggler:first-child").toggleClass("animation-toggler-1")
     $(".button-toggler:nth-child(2)").toggleClass("animation-toggler-2")
-    $(".button-toggler:nth-child(3)").toggleClass("animation-toggler-3")
-      ;
+    $(".button-toggler:nth-child(3)").toggleClass("animation-toggler-3");
   });
   // navbar
   $(window).scroll(function () {
